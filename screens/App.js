2097d2f289214a60aa535a7ded046519d1826f8a
Shariq Shahid <shariqshahid905@gmail.com>
2022-08-25 17:14:28 +0500

    Pages

--- screens/App.js
+++ screens/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import 'react-native-gesture-handler';
 import Home from './Home';
 import NewTweet from './NewTweet';
@@ -13,43 +13,82 @@ import Search from './Search';
 import Notification from './Notification';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
-
 const Stack = createStackNavigator();
 const Drawer = createDrawerNavigator();
 const Tab = createBottomTabNavigator();
 
 const HomeStackNavigator = () => {
   return (
-    <Stack.Navigator>
-      <Stack.Screen name="Tab" component={TabNavigator} />
-      <Stack.Screen name="New Tweet" component={NewTweet} />
-      <Stack.Screen name="Tweet Screen" component={TweetScreen} />
-      <Stack.Screen name="Profile" component={Profile} />
+    <Stack.Navigator
+      screenOptions={{headerShown: true, headerBackTitleVisible: false}}>
+      <Stack.Screen
+        name="Tab"
+        component={TabNavigator}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="New Tweet"
+        component={NewTweet}
+        options={{title: ''}}
+      />
+      <Stack.Screen
+        name="Tweet Screen"
+        component={TweetScreen}
+        options={{title: ''}}
+      />
+      <Stack.Screen name="Profile" component={Profile} options={{title: ''}} />
     </Stack.Navigator>
-  )
-}
+  );
+};
 
 const TabNavigator = () => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home1" component={Home} options={{
-        tabBarIcon: ({color, size}) => (
-          <FontAwesome name="home" size={size} color={color} />
-        )
-      }} />
-      <Tab.Screen name="Search" component={Search} />
-      <Tab.Screen name="Notification" component={Notification} />
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: false,
+      }}>
+      <Tab.Screen
+        name="Home1"
+        component={Home}
+        options={{
+          tabBarIcon: ({color, size}) => (
+            <FontAwesome name="home" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Search"
+        component={Search}
+        options={{
+          tabBarIcon: ({color, size}) => (
+            <FontAwesome name="search" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Notification"
+        component={Notification}
+        options={{
+          tabBarIcon: ({color, size}) => (
+            <FontAwesome name="bell" size={size} color={color} />
+          ),
+        }}
+      />
     </Tab.Navigator>
-  )
-}
+  );
+};
 
 export default function App() {
-  return(
+  return (
     <NavigationContainer>
-      <Drawer.Navigator>
+      <Drawer.Navigator
+        screenOptions={{
+          headerShown: true,
+        }}>
         <Drawer.Screen name="Home" component={HomeStackNavigator} />
         <Drawer.Screen name="Settings" component={SettingsScreen} />
       </Drawer.Navigator>
     </NavigationContainer>
-  )
+  );
 }
--- screens/Home/index.js
+++ screens/Home/index.js
@@ -1,22 +1,72 @@
-import React from "react";
-import { Button, StyleSheet, Text, View } from "react-native";
-import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import React from 'react';
+import {FlatList, StyleSheet, Text, View} from 'react-native';
 
 const Home = ({navigation}) => {
-  return (
+  const Data = [
+    {
+      id: '1',
+      title: 'First Item',
+    },
+    {
+      id: '2',
+      title: 'Second Item',
+    },
+    {
+      id: '3',
+      title: 'Third Item',
+    },
+    {
+      id: '4',
+      title: 'Fourth Item',
+    },
+    {
+      id: '5',
+      title: 'Fifth Item',
+    },
+    {
+      id: '6',
+      title: 'Sixth Item',
+    },
+    {
+      id: '7',
+      title: 'Seventh Item',
+    },
+    {
+      id: '8',
+      title: 'Eight Item',
+    },
+    {
+      id: '9',
+      title: 'Ninth Item',
+    },
+    {
+      id: '10',
+      title: 'Tenth Item',
+    },
+  ];
+
+  const renderItem = ({item}) => (
     <View>
-      <Text>
-        <FontAwesome name='home' size={20} />
-        Home
-      </Text>
-      
-      <Button title="New Tweet" onPress={() => navigation.navigate('New Tweet')}/>
-      <Button title="Tweet Screen" onPress={() => navigation.navigate('Tweet Screen')}/>
-      <Button title="Profile" onPress={() => navigation.navigate('Profile')}/>
+      <Text>{item.title}</Text>
     </View>
-  )
-}
+  );
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={Data}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+      />
+    </View>
+  );
+};
 
 export default Home;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffffff',
+  },
+});
