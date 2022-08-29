4305e59097ff0f57f46ec48de33299f80af71320
“srq” <shariqshahid905@gmail.comgit add origin to maingit initgit addgit config --global user.email shariqshahid905@gmail.com>
2022-08-29 19:16:31 +0500

    Single Tweet Design

--- screens/TweetScreen/index.js
+++ screens/TweetScreen/index.js
@@ -1,14 +1,135 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React from 'react';
+import {Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import Entypo from 'react-native-vector-icons/Entypo';
+import EvilIcons from 'react-native-vector-icons/EvilIcons';
 
 const TweetScreen = () => {
   return (
-    <View>
-      <Text>Tweet Screen</Text>
+    <View style={styles.container}>
+      <View style={styles.profileContainer}>
+        <TouchableOpacity style={styles.flexRow}>
+          <Image
+            style={styles.avatar}
+            source={{
+              uri: 'https://reactnative.dev/img/tiny_logo.png',
+            }}
+          />
+          <View>
+            <Text style={styles.tweetName}>Shariq Shahid</Text>
+            <Text style={styles.tweetHandle}>@srq95</Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Entypo name="dots-three-vertical" size={24} color="gray" />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.tweetContentContainer}>
+        <Text style={styles.tweetContent}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </Text>
+      </View>
+      <View style={styles.tweetEngagement}>
+        <View style={styles.flexRow}>
+          <Text style={styles.tweetEngagementNumber}>628</Text>
+          <Text style={styles.tweetEngagementLabel}>Reteets</Text>
+        </View>
+        <View style={[styles.flexRow, styles.ml4]}>
+          <Text style={styles.tweetEngagementNumber}>38</Text>
+          <Text style={styles.tweetEngagementLabel}>Quote Tweets</Text>
+        </View>
+        <View style={[styles.flexRow, styles.ml4]}>
+          <Text style={styles.tweetEngagementNumber}>2,934</Text>
+          <Text style={styles.tweetEngagementLabel}>Likes</Text>
+        </View>
+      </View>
+      <View style={[styles.tweetEngagement, styles.spaceAround]}>
+        <TouchableOpacity style={styles.flexRow}>
+          <EvilIcons name="comment" size={32} style={styles.textGrey} />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.flexRow}>
+          <EvilIcons name="retweet" size={32} style={styles.textGrey} />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.flexRow}>
+          <EvilIcons name="heart" size={32} style={styles.textGrey} />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.flexRow}>
+          <EvilIcons
+            name={Platform.OS === 'ios' ? 'share-apple' : 'share-google'}
+            size={32}
+            style={styles.textGrey}
+          />
+        </TouchableOpacity>
+      </View>
     </View>
-  )
-}
+  );
+};
 
 export default TweetScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffffff',
+  },
+  profileContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingVertical: 12,
+    paddingHorizontal: 10,
+  },
+  flexRow: {
+    flexDirection: 'row',
+  },
+  avatar: {
+    width: 50,
+    height: 50,
+    marginRight: 8,
+    borderRadius: 25,
+  },
+  tweetName: {
+    fontWeight: 'bold',
+    color: '#222222',
+  },
+  tweetHandle: {
+    color: 'gray',
+  },
+  tweetContentContainer: {
+    paddingHorizontal: 10,
+    paddingBottom: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#e5e7eb',
+  },
+  tweetContent: {
+    fontSize: 20,
+    lineHeight: 30,
+    color: '#222222',
+  },
+  tweetEngagement: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 10,
+    paddingVertical: 12,
+    borderBottomWidth: 1,
+    borderBottomColor: '#e5e7eb',
+  },
+  tweetEngagementNumber: {
+    fontWeight: 'bold',
+    color: '#222222',
+  },
+  tweetEngagementLabel: {
+    color: 'gray',
+    marginLeft: 6,
+  },
+  ml4: {
+    marginLeft: 15,
+  },
+  textGrey: {
+    color: 'gray',
+  },
+  spaceAround: {
+    justifyContent: 'space-around',
+  }
+});
